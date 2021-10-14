b6ade19eaf4decae0d1074a819baf4c3c45b80ba
Dennis Voltz <dennis.voltz@wwu.de>
2021-10-14 12:30:44 +0200

    Remove some boilercode

--- components/Home.vue
+++ components/Home.vue
@@ -42,22 +42,6 @@
           </a>
         </v-row>
       </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
     </v-row>
   </div>
 </template>
@@ -67,20 +51,6 @@ export default {
   name: "Home",
 
   data: () => ({
-    ecosystem: [
-      {
-        text: "vuetify-loader",
-        href: "https://github.com/vuetifyjs/vuetify-loader",
-      },
-      {
-        text: "github",
-        href: "https://github.com/vuetifyjs/vuetify",
-      },
-      {
-        text: "awesome-vuetify",
-        href: "https://github.com/vuetifyjs/awesome-vuetify",
-      },
-    ],
     importantLinks: [
       {
         text: "Documentation",
